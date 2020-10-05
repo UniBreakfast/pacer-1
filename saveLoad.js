4815e3ac557843165d6ec05bd0f8f6b2f66a20be
SerhiiShymchuk <shymaser@gmail.com>
2020-10-05 08:59:40 +0300

    load state, save state

--- saveLoad.js
+++ saveLoad.js
@@ -1,6 +1,7 @@
 const saveLoadBar = document.createElement('div')
 saveLoadBar.innerHTML = `
-    <a class="button" href="javascript:saveState()" download="" style="text-decoration:none">Save</a>
+    <button onclick="saveState()">Save</button>   
+    <a href="" download="" hidden></a>
     <button onclick="this.nextElementSibling.click()" >Load</button>
     <input type="file" onchange="loadState()" hidden>
 `
@@ -14,18 +15,19 @@ Object.assign(saveLoadBar.style, {
 
 // функція для самостійного кліка на анкор і зберігання файлу
 function saveState() {
-    const state = {activities, 
+    const state = {
+        activities, 
         quests, 
         todos, 
         confidence: localStorage.confidence, 
-        nextID: localStorage.nextID,}
+        nextID: localStorage.nextID,
+    }
     const contentLink = "data:text/plain;charset=utf-8," + encodeURI(JSON.stringify(state,0,2))
     const fileName = String(new Date).match(/ ([A-Z].+:\d\d)/)[1].replace(/:/g,'-')+'.json'
     const a = saveLoadBar.querySelectorAll('a')[0]
     a.href = contentLink
     a.download = fileName
     a.click()
-    a.href = "javascript:saveState()"
 }
 // функція для завантаження прогресу з файлу
 function loadState() {
