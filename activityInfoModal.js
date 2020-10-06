857e69154c5324a9ff3b70808efbd231485e5470
SerhiiShymchuk <shymaser@gmail.com>
2020-10-06 10:53:17 +0300

    activityInfoModal end

--- activityInfoModal.js
+++ activityInfoModal.js
@@ -1,6 +1,13 @@
 activityInfoGlass.onclick = event => {
     if (event.target == activityInfoGlass || event.target.classList.contains('close'))
-    activityInfoGlass.hidden = true
+        activityInfoGlass.hidden = true
+    else if (event.target.classList.contains('save')) {
+        if (updateActivity(activityInfoModal.querySelector('.save').dataset.id)) {
+            showActivities()
+            showQuests()
+            activityInfoGlass.hidden = true
+        }
+    }
 }
 
 
@@ -11,13 +18,16 @@ function prepActivityInfoModal(activity) {
     inputs[1].value = activity.size
     inputs[2].value = activity.diff
 
+    const activityQuests = quests.filter(quest => quest.activityID == activity.id)
+    activityInfoModal.querySelector('ul').innerHTML = 
+        activityQuests.map(buildActivityQuest).join('')
+    
+    activityInfoModal.querySelector('.save').dataset.id = activity.id
 }
 // функція для показу модального вікна  при натисканні Деталі
 function showActivityInfoModal(activityID) {
     const activity = activities.find(activity => activity.id == activityID)
     prepActivityInfoModal(activity)
-    const activityQuests = quests.filter(quest => quest.activityID == activity.id)
-    activityInfoModal.querySelector('ul').innerHTML = activityQuests.map(buildActivityQuest).join('')
     activityInfoGlass.hidden = false
 }
 //функція для побудови розмітки деталей квесту
@@ -37,5 +47,25 @@ function buildActivityQuest(quest) {
         </li>
     `
 }
+//функція для перевірки інпутів та оновлення значень дыяльності
+function updateActivity(activityID) {
+    //const inputs = [...activityInfoModal.querySelectorAll('input')]
+    const inputs = activityInfoModal.querySelectorAll('input')
+    const activity = activities.find(activity => activity.id == activityID)
+    if (inputs[0].value == activity.name &&
+        inputs[1].value == activity.size &&
+        inputs[2].value == activity.diff) return true
+        
+    //if (inputs.some(input => input.value == '')) alert('Заповніть пусті поля')
+    if ([inputs[0].value, inputs[1].value, inputs[2].value].includes('')) {
+        showAlert('Заповніть пусті поля')
+        return false
+    }
 
-    
+    activity.name = inputs[0].value
+    activity.size = inputs[1].value
+    const diff = Math.min(Math.max(1, +inputs[2].value), 10)
+    activity.diff = diff
+    localStorage.activities = JSON.stringify(activities)
+    return true
+}
